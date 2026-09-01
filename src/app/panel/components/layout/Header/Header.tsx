8423b12562c37292e4bd440a84a28970fc70b8a8
mahzadkhosraviani <khosravianimahzad@gmail.com>
2026-09-01 15:01:30 +0330

    responsive bug fixes and adding hamburger menu

--- src/app/panel/components/layout/Header/Header.tsx
+++ src/app/panel/components/layout/Header/Header.tsx
@@ -3,22 +3,26 @@
 import {
 	ActionIcon,
 	Box,
-	Button,
+	Burger,
+	Drawer,
 	Flex,
 	Group,
-	NativeSelect,
+	Select,
 	Stack,
 	Text,
 	useMantineColorScheme,
 } from "@mantine/core";
-
-import { IconMoonFilled } from "@tabler/icons-react";
+import { IconMoonFilled, IconSunFilled } from "@tabler/icons-react";
+import { useState } from "react";
+import AuthButton from "@/app/panel/components/layout/Header/authButton";
 import { useCurrentPage, useDataRange } from "@/app/panel/components/layout/Header/index.hooks";
+import Sidebar from "@/app/panel/components/layout/Sidebar";
 
 export default function Header() {
+	const [opened, setOpened] = useState(false);
 	const currentPage = useCurrentPage();
 	const { dateRange, setDateRange, dateLabel } = useDataRange();
-	const { toggleColorScheme } = useMantineColorScheme();
+	const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 	return (
 		<Flex
 			h="100%"
@@ -30,7 +34,6 @@ export default function Header() {
 				lg: "lg",
 			}}
 		>
-			{/* Left side */}
 			<Flex
 				align="center"
 				gap={{
@@ -40,9 +43,8 @@ export default function Header() {
 				}}
 				miw={0}
 			>
-				{/* Logo */}
 				<Group gap="md" wrap="nowrap">
-					<Box bg="layout.5" w={32} h={32} className="flex shrink-0 items-center justify-center rounded-lg ">
+					<Box bg="layout.5" w={34} h={34} className="flex shrink-0 items-center justify-center rounded-lg ">
 						<Text fw={700} c="black">
 							F
 						</Text>
@@ -53,37 +55,59 @@ export default function Header() {
 					</Text>
 				</Group>
 
-				{/* Page title */}
-				<Stack gap={2} miw={0}>
-					<Text size="xl" fw={700}>
+				<Stack ml={{ base: "xs", sm: "sm" }} gap={2} miw={0}>
+					<Text className={"text-lg! sm:text-2xl! "} fw={700}>
 						{currentPage.title}
 					</Text>
 
-					<Text size="sm" c="dimmed" truncate>
+					<Text className={"text-sm! sm:text-lg! "} c="dimmed">
 						{currentPage.description}, {dateLabel}
 					</Text>
 				</Stack>
 			</Flex>
 
-			{/* Right side */}
 			<Group gap="xs" wrap="nowrap">
-				<Button>Sign up</Button> <Button>Sign In</Button>
-				<NativeSelect
+				<Burger hiddenFrom="sm" opened={opened} onClick={() => setOpened((value) => !value)} size="sm" />
+				<Drawer
+					styles={{
+						title: {
+							color: "var(--mantine-color-layout-4)",
+							fontSize: "24px",
+							fontWeight: 700,
+						},
+					}}
+					opened={opened}
+					onClose={() => setOpened(false)}
+					title="FinFlow"
+					hiddenFrom="sm"
+				>
+					<Stack gap="md">
+						<Sidebar />
+
+						<Stack gap="xs">
+							<AuthButton />
+						</Stack>
+					</Stack>
+				</Drawer>
+				<Group visibleFrom="sm" gap="xs" wrap="nowrap">
+					<AuthButton />
+				</Group>
+				<Select
+					w={{
+						base: 80,
+						sm: 140,
+					}}
 					value={dateRange}
-					onChange={(event) => setDateRange(event.currentTarget.value)}
+					onChange={(value) => setDateRange(value ?? "this-month")}
 					data={[
 						{ value: "this-month", label: "This month" },
 						{ value: "last-month", label: "Last month" },
 						{ value: "this-year", label: "This year" },
 						{ value: "last-year", label: "Last year" },
 					]}
-					w={{
-						base: 110,
-						sm: 140,
-					}}
 				/>
-				<ActionIcon onClick={toggleColorScheme} variant="filled" size="lg">
-					<IconMoonFilled size={18} />
+				<ActionIcon onClick={toggleColorScheme} variant="filled" size="lg" color={"layout"}>
+					{colorScheme === "dark" ? <IconSunFilled size={22} /> : <IconMoonFilled size={18} />}
 				</ActionIcon>
 			</Group>
 		</Flex>
---
+++ src/app/panel/components/layout/Header/authButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mantine/core";
+
+export default function AuthButton() {
+	return (
+		<>
+			<Button color={"layout"}>Sign up</Button> <Button color={"layout"}>Sign In</Button>
+		</>
+	);
+}
